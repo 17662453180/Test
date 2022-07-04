760587199984b9b4598d12c3ba209309e3eb8388
Zhangwenwen <zww970813>
2022-07-04 17:05:42 +0800

    二次修改

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }} -- zww</div>
+    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">name: {{ name }}</div>
   </div>
 </template>
 
