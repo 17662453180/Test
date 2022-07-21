47a6dbcfd9273f4fdc1696ba6e8a5bd6c7140d24
zhang wenwen <1738093828@qq.com>
2022-07-21 17:03:34 +0800

    测试提交

---
+++ src/components/test/slot-test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <slot :user="user">{{ user.name }}</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlotTest',
+  components: {
+  },
+  data() {
+    return {
+      user: {
+        name: 'zww',
+        age: '18'
+      }
+    }
+  },
+  computed: {
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang='scss'>
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,18 +4,71 @@
     <div class="dashboard-text">name: {{ name }}</div>
     <div class="dashboard-text">name: {{ name }}</div>
     <div class="dashboard-text">name: {{ name }}</div>
+    <p v-for="item in items" :key="item.id">
+      {{ item.message }}
+    </p>
+    <el-button @click="test">this.$set测试</el-button>
+    <SlotTest>
+      <template slot-scope="slotProps">{{ slotProps.user.age }}</template>
+    </SlotTest>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import SlotTest from '@/components/test/slot-test'
 
 export default {
   name: 'Dashboard',
+  components: {
+    SlotTest
+  },
   computed: {
     ...mapGetters([
       'name'
     ])
+  },
+  data() {
+    return {
+      obj: {
+        name: '123'
+      },
+      items: [
+        { message: 'one', id: '1' },
+        { message: 'two', id: '2' },
+        { message: 'three', id: '3' }
+      ]
+    }
+  },
+  created() {
+    window.myData = this
+    const obj1 = {
+      name: 'jack',
+      sex: undefined,
+      age: 25,
+      hobby: {
+        ball: 'tennis'
+      }
+    }
+    const obj2 = Object.assign({}, obj1)
+    // const obj2 = JSON.parse(JSON.stringify(obj1))
+
+    // obj2.hobby.ball = 'basketball'
+    // obj2.name = 'zww'
+    const arr1 = ['1', '2', ['a', 'b']]
+    const arr2 = [...arr1]
+    arr2[2][0] = 'zww'
+    // console.log('arr1', arr1) // basketball
+    // console.log('arr2', arr2) // basketball
+    console.log('obj1', obj1) // basketball
+    // console.log('obj2', obj2) // basketball
+  },
+  methods: {
+    test() {
+      this.items[0] = { message: 'first', id: '4' }
+      this.$set(this.items, 0, { message: 'first', id: '4' })
+      console.log('shuchu', this.items)
+    }
   }
 }
 </script>
